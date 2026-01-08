9680e213e92eb9a74e1a1687b2f738c62119be8b
patrickmullins <pmullins@byui.edu>
2026-01-07 21:22:58 -0500

    Pushed the unit01 prep1 program to the correct file / folder

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,13 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {  
+        static void Main(string[] args)
+        {
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.Write("Your name is " + lastName + ", " + firstName + " " + lastName + ".");
+        }
     }
-}

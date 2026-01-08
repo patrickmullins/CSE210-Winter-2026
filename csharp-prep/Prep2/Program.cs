102c889da00570b53c243ca7d37d813779e208a3
patrickmullins <pmullins@byui.edu>
2026-01-07 22:21:45 -0500

    C# Prep 2 program completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+
+        string letter = "";
+         if (gradeNumber >= 93)
+            {
+                letter = "A";
+            }
+        else if (gradeNumber >= 90)
+            {
+                letter = "A-";
+            }
+        else if (gradeNumber >= 87)
+            {
+                letter = "B+";
+            }
+        else if (gradeNumber >= 83)
+            {
+                letter = "B";
+            }
+        else if (gradeNumber >= 80)
+            {
+                letter = "B-";
+            }
+        else if (gradeNumber >= 77)
+            {
+                letter = "C+";
+            }
+        else if (gradeNumber >= 73)
+            {
+                letter = "C";
+            }
+        else if (gradeNumber >= 70)
+            {
+                letter = "C-";
+            }
+        else if (gradeNumber >= 67)
+            {  
+                 letter = "D+";
+            }
+        else if (gradeNumber >= 63)
+            {  
+                 letter = "D";
+            }
+        else if (gradeNumber >= 60)
+            { 
+                 letter = "D-";
+            }
+        else
+            {
+                 letter = "F";
+            }
+        
+        Console.WriteLine($"Your grade is: " + letter);
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying, you were close to passing!");
+        }
     }
 }
